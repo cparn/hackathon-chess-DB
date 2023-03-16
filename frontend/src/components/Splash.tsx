bd054ab052e8b8787ed824177b6cfdd2958ec26a
cparn <caspar.rodoo@gmail.com>
2023-03-16 22:27:17 +0100

    css DONE? starting splash

--- frontend/src/components/Splash.tsx
+++ frontend/src/components/Splash.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import './Splash.css'
 
 export const Splash: React.FC = () => {
     return (
-        <main>hello</main>
+        <section>
+            <div className="splash-card">
+                <h2>Welcome to MyChessDB</h2>
+            </div>
+            <div className="splash-media">
+                <h3>How it works</h3>
+                <p>Find and review your favorite chess games or share your own.</p>
+            </div>
+        </section>
     )
 }
