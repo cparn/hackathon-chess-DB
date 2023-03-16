b1e50ea7674a2da5f2226e85a5d268cf3c1bb595
cparn <caspar.rodoo@gmail.com>
2023-03-16 09:29:51 +0100

    gamecard work, need to implement location to model

--- frontend/src/components/GamesList/GameCard.tsx
+++ frontend/src/components/GamesList/GameCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Game } from './GamesList'
+import './GameCard.css'
 
 type GameCardProps = {
     game: Game
@@ -9,6 +10,12 @@ type GameCardProps = {
 export const GameCard: React.FC<GameCardProps> = ({ game }) => {
 
     return (
-        <Link to="/game-viewer" state={{ data: game }}><h3>{game.opponent}</h3></Link>)
+        <Link to="/game-viewer" state={{ data: game }}>
+            <div className='game-card'>
+                <h3>{game.opponent}</h3>
+                <p>Played at:</p>
+                <p>{game.</p>
+            </div>
+        </Link>)
 }
 
