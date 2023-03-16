d7e5e8e397f61a14db505c223ea420ad3643e88d
cparn <caspar.rodoo@gmail.com>
2023-03-16 10:40:34 +0100

    Form workä

--- frontend/src/components/GamesList/EditGameForm.tsx
+++ frontend/src/components/GamesList/EditGameForm.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import { Game } from './GamesList'
+import React, { useState } from 'react';
+import { Game } from './GamesList';
+import './EditGameForm.css';
 
 type EditGameFormProps = {
     game: Game
@@ -20,8 +21,31 @@ export const EditGameForm: React.FC<EditGameFormProps> = ({ game }) => {
     });
 
     return (
-        <form>
-
-        </form>
+        <>
+            <h3>Edit game</h3>
+            <form className="edit-form">
+                <div className="edit-form__field">
+                    <label htmlFor="editOpponent">Opponent</label>
+                    <input id="editOpponent" type="text" value={formState.opponent}>
+                    </input>
+                </div>
+                <div className="edit-form__field">
+                    <label htmlFor="editDate">Date</label>
+                    <input id="editDate" type="text" value={formState.date}>
+                    </input>
+                </div>
+                <div className="edit-form__field">
+                    <label htmlFor="editLocation">Location</label>
+                    <input id="editLocation" type="text" value={formState.location}>
+                    </input>
+                </div>
+                <div className="edit-form__field">
+                    <label htmlFor="editComments">Comments</label>
+                    <input id="editComments" type="text" value={formState.comments}>
+                    </input>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </>
     )
 }
