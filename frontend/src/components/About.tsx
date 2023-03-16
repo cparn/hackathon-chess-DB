15f6218bd0860618d211fa330ecabe9b436ccba8
cparn <caspar.rodoo@gmail.com>
2023-03-16 22:56:23 +0100

    home and about finished

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const About: React.FC = () => {
     return (
-        <p>about</p>
+        <section>
+            <div className="splash-media">
+                <h2>About</h2>
+                <p>
+                    {"This website was built for </salt> hackday, an event where we had 24 hours to build a fullstack web application."}
+                    <br></br><br></br>
+                    {"It uses SqlServer as its database, Entity Framework and .NET WebAPI for backend, and React for frontend. This provides a seamless and responsive user experience, and the ability to store games securely and reliably."}
+                    <br></br><br></br>
+                    {"Some of the things I wish I had time to finish include styling, improving the chess engine, and adding filter features for game listings."}
+                    <br></br><br></br>
+                    {"Thank you for visiting my website!"}</p>
+            </div>
+        </section>
     )
 }
--- frontend/src/components/Splash.tsx
+++ frontend/src/components/Splash.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Splash.css'
 
 export const Splash: React.FC = () => {
@@ -8,9 +9,20 @@ export const Splash: React.FC = () => {
                 <h2>Welcome to MyChessDB</h2>
             </div>
             <div className="splash-media">
-                <h3>How it works</h3>
-                <p>Find and review your favorite chess games or share your own.</p>
+                <h3>What is this</h3>
+                <p>Welcome to <span className="highlight">MyChessDB</span>, a platform designed for the storage and
+                    accessibility of your chess games in a secure and user-friendly
+                    database. This platform offers a convenient means of tracking your
+                    progress, analyzing your performance, and sharing your games with
+                    others, regardless of your level of experience with the game.
+                    The intuitive interface and powerful search tools provide a seamless user experience
+                    and allow you to easily find and review the games you desire. Our passion for chess
+                    drives us to assist you in your goal of improving your skills.
+                    We invite you to sign up today and start building your personal chess archive with MyChessDB.</p>
             </div>
+            <Link to="/games">
+                <button className="cta">Get started!</button>
+            </Link>
         </section>
     )
 }
