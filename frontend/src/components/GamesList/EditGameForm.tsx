a01c3d8bcd4fe5582c6a5cc0a3d8da6aae3be247
cparn <caspar.rodoo@gmail.com>
2023-03-16 11:05:54 +0100

    form work

--- frontend/src/components/GamesList/EditGameForm.tsx
+++ frontend/src/components/GamesList/EditGameForm.tsx
@@ -20,28 +20,73 @@ export const EditGameForm: React.FC<EditGameFormProps> = ({ game }) => {
         comments: game.comments
     });
 
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        setFormState(prevState => ({ ...prevState, [name]: value }));
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const body = {
+            gameId: game.gameId,
+            opponent: formState.opponent,
+            location: formState.location,
+            date: formState.date,
+            gamePGN: game.gamePGN,
+            comments: formState.comments
+        }
+        const putResponse = await fetch(`http://localhost:5172/api/ChessGame/${game.gameId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body)
+        });
+        const result = await putResponse.json();
+        if(result.error){
+            alert('bad');
+        }
+    }
+
     return (
         <>
             <h3>Edit game</h3>
-            <form className="edit-form">
+            <form className="edit-form" onSubmit={handleSubmit}>
                 <div className="edit-form__field">
                     <label htmlFor="editOpponent">Opponent</label>
-                    <input id="editOpponent" type="text" value={formState.opponent}>
+                    <input id="editOpponent"
+                        type="text"
+                        name="opponent"
+                        value={formState.opponent}
+                        onChange={handleInputChange}>
                     </input>
                 </div>
                 <div className="edit-form__field">
                     <label htmlFor="editDate">Date</label>
-                    <input id="editDate" type="text" value={formState.date}>
+                    <input id="editDate"
+                        type="text"
+                        name="date"
+                        value={formState.date}
+                        onChange={handleInputChange}>
                     </input>
                 </div>
                 <div className="edit-form__field">
                     <label htmlFor="editLocation">Location</label>
-                    <input id="editLocation" type="text" value={formState.location}>
+                    <input id="editLocation"
+                        type="text"
+                        name="location"
+                        value={formState.location}
+                        onChange={handleInputChange}>
                     </input>
                 </div>
                 <div className="edit-form__field">
                     <label htmlFor="editComments">Comments</label>
-                    <input id="editComments" type="text" value={formState.comments}>
+                    <input id="editComments"
+                        type="text"
+                        name="comments"
+                        value={formState.comments}
+                        onChange={handleInputChange}>
                     </input>
                 </div>
                 <button type="submit">Submit</button>
