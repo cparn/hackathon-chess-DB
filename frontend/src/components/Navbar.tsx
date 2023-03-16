8bb6af8c0394fbb9ee3fc7947cc9ab148bc5d6d3
cparn <caspar.rodoo@gmail.com>
2023-03-16 11:55:00 +0100

    Changed title

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import './Navbar.css';
 export const Navbar: React.FC = () => {
     return (
         <header>
-            <h1>Personal chess explorer</h1>
+            <h1>MyChessDB</h1>
             <div className='navbar'>
                 <Link to="/">Home</Link>
                 <Link to="/games">Games</Link>
