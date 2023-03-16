c6effaf870897febcf768de759af6094ee384b25
cparn <caspar.rodoo@gmail.com>
2023-03-16 11:10:01 +0100

    Put model require inputs

--- backend/Models/ChessGame.cs
+++ backend/Models/ChessGame.cs
@@ -5,10 +5,16 @@ namespace backend.Models;
 public class ChessGame
 {
     [Key]
+    [Required]
     public int GameId { get; set; }
+    [Required]
     public string Opponent { get; set; }
+    [Required]
     public string Location { get; set; }
+    [Required]
     public string Date { get; set; }
+    [Required]
     public string GamePGN { get; set; }
+    [Required]
     public string? Comments { get; set; }
 }
