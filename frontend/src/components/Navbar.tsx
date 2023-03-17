2d358f99897e2775bd931bc968a8b941fbe7f053
cparn <caspar.rodoo@gmail.com>
2023-03-17 09:50:59 +0100

    cleanup css

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,7 +8,7 @@ export const Navbar: React.FC = () => {
             <h1>MyChessDB</h1>
             <div className='navbar'>
                 <Link to="/">Home</Link>
-                <Link to="/games">Games</Link>
+                <Link to="/games">My Games</Link>
                 <Link to="/add">Add Game</Link>
                 <Link to="/about">About</Link>
             </div>
